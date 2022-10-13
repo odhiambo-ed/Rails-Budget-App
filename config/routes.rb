b317811dd9c8913bfeb7ac3a2c0c388df3a128f8
white3d <odhiambo.edward111@gmail.com>
2022-10-13 15:49:55 +0300

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :expenses
-  resources :categories
+  resources :categories, only:[:index, :new, :create] do
+    resources :expenses, only:[:index, :new, :create]
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
