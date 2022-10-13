c403677c5fe00563e21807662b4cbfee37d76902
white3d <odhiambo.edward111@gmail.com>
2022-10-13 15:51:58 +0300

    Edit Expenses Controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,11 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_expense, only: %i[ show edit update destroy ]
 
   # GET /expenses or /expenses.json
   def index
-    @expenses = Expense.all
+    @expenses = Expense.where(category_id: params[:category_id]).order('created_at DESC')
+    @id = params[:category_id]
   end
 
   # GET /expenses/1 or /expenses/1.json
@@ -12,7 +14,7 @@ class ExpensesController < ApplicationController
 
   # GET /expenses/new
   def new
-    @expense = Expense.new
+    @id = params[:category_id]
   end
 
   # GET /expenses/1/edit
@@ -21,31 +23,28 @@ class ExpensesController < ApplicationController
 
   # POST /expenses or /expenses.json
   def create
-    @expense = Expense.new(expense_params)
-
-    respond_to do |format|
-      if @expense.save
-        format.html { redirect_to expense_url(@expense), notice: "Expense was successfully created." }
-        format.json { render :show, status: :created, location: @expense }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
+    expense = Expense.new(expense_params)
+    expense.user_id = current_user.id
+    redirect_to category_transactions_path(params[:expense][:category_id])
+    if expense.save
+      flash[:notice] = 'transaction created successfully'
+    else
+      flash[:alert] = 'Error occurred while creating the transaction'
     end
   end
 
   # PATCH/PUT /expenses/1 or /expenses/1.json
-  def update
-    respond_to do |format|
-      if @expense.update(expense_params)
-        format.html { redirect_to expense_url(@expense), notice: "Expense was successfully updated." }
-        format.json { render :show, status: :ok, location: @expense }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @expense.update(expense_params)
+  #       format.html { redirect_to expense_url(@expense), notice: "Expense was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @expense }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @expense.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /expenses/1 or /expenses/1.json
   def destroy
@@ -65,6 +64,6 @@ class ExpensesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def expense_params
-      params.require(:expense).permit(:name, :amount, :user_id, :category_id)
+      params.require(:expense).permit(:name, :amount, :category_id)
     end
 end
