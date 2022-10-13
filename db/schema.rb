f939e6b6762d9abca4c94875757daf91ab7a1b25
white3d <odhiambo.edward111@gmail.com>
2022-10-13 20:42:48 +0300

    Remove User_Id From Expenses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_173153) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_174038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,12 +26,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_173153) do
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
-    t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_expenses_on_category_id"
-    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,5 +48,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_173153) do
 
   add_foreign_key "categories", "users"
   add_foreign_key "expenses", "categories"
-  add_foreign_key "expenses", "users"
 end
