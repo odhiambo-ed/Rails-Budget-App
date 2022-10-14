feda6176aaacfd75900f35e0d29bf6a8b8f4b1bd
white3d <odhiambo.edward111@gmail.com>
2022-10-14 10:41:18 +0300

    Add Validation To User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
   has_many :categories
+  has_many :expenses, through: :categories
 end
