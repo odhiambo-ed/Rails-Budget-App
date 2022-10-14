4dea4145c62027ad692a7a7908f32bb9cb4861e9
white3d <odhiambo.edward111@gmail.com>
2022-10-14 15:13:59 +0300

    Add Expense Model Spec

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: 'Edward Odhiambo', email: 'edward@gmail.com', password: 'test123',
+                     password_confirmation: 'test123')
+
+    @user.save
+
+    @category = Category.new(name: 'Food Stuff')
+
+    @category.save
+
+     @expense = Expense.new(name: 'Cooking Oil', amount: 2000)
+
+    @expense.save
+
+    @category.expenses << @expense
+
+    @user.categories << @category
+  end
+
+  it 'should have a name' do
+    @expense.name
+    expect(@expense.name).to eq('Cooking Oil')
+  end
+
+  it 'should have an amount' do
+    @expense.amount
+    expect(@expense.amount).to eq(2000)
+  end
+
+  it 'should belong to a group' do
+    @expense.categories
+    expect(@expense.categories).to eq(@expense.categories)
+  end
 end
