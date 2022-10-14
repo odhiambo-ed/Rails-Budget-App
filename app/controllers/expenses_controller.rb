edb30466dc3dbe4e675bbac8d77985e46b17e991
white3d <odhiambo.edward111@gmail.com>
2022-10-14 11:56:01 +0300

    Edit Expenses Controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,69 +1,69 @@
 class ExpensesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_expense, only: %i[ show edit update destroy ]
+    before_action :authenticate_user!
 
-  # GET /expenses or /expenses.json
   def index
-    @expenses = Expense.where(category_id: params[:category_id]).order('created_at DESC')
-    @id = params[:category_id]
+    @expenses = Expense.all
   end
 
-  # GET /expenses/1 or /expenses/1.json
-  def show
-  end
+  # GET /entities/1 or /entities/1.json
+  def show; end
 
-  # GET /expenses/new
+  # GET /entities/new
   def new
-    @id = params[:category_id]
+    @expense = Expense.new
   end
 
-  # GET /expenses/1/edit
-  def edit
-  end
+  # GET /entities/1/edit
+  def edit; end
 
-  # POST /expenses or /expenses.json
+  # POST /entities or /entities.json
   def create
-    expense = Expense.new(expense_params)
-    expense.user_id = current_user.id
-    redirect_to category_expenses_path(params[:expense][:category_id])
-    if expense.save
-      flash[:notice] = 'Expense created successfully'
-    else
-      flash[:alert] = 'Error occurred while creating the expense'
+    @expense = Expense.new(params.require(:expense).permit(:name, :amount, :user_id, :category_id))
+    # @entity = Entity.new(entity_params)
+
+    respond_to do |format|
+      if @expense.save
+        format.html { redirect_to categories_url, notice: 'Expense was successfully created.' }
+        format.json { render :show, status: :created, location: @expense }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  # PATCH/PUT /expenses/1 or /expenses/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @expense.update(expense_params)
-  #       format.html { redirect_to expense_url(@expense), notice: "Expense was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @expense }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @expense.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  # PATCH/PUT /entities/1 or /entities/1.json
+  def update
+    respond_to do |format|
+      if @expense.update(expense_params)
+        format.html { redirect_to expense_url(@expense), notice: 'Expense was successfully updated.' }
+        format.json { render :show, status: :ok, location: @expense }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  # DELETE /expenses/1 or /expenses/1.json
+  # DELETE /entities/1 or /entities/1.json
   def destroy
     @expense.destroy
 
     respond_to do |format|
-      format.html { redirect_to expenses_url, notice: "Expense was successfully destroyed." }
+      format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_expense
-      @expense = Expense.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def expense_params
-      params.require(:expense).permit(:name, :amount, :category_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :user_id, :category_id)
+  end
 end
--- spec/routing/expenses_routing_spec.rb
+++ spec/routing/expenses_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe ExpensesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/expenses").to route_to("expenses#index")
+      expect(get: "category/expenses").to route_to("expenses#index")
     end
 
     it "routes to #new" do
-      expect(get: "/expenses/new").to route_to("expenses#new")
+      expect(get: "category/expenses/new").to route_to("expenses#new")
     end
 
     it "routes to #show" do
-      expect(get: "/expenses/1").to route_to("expenses#show", id: "1")
+      expect(get: "category/expenses/1").to route_to("expenses#show", id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/expenses/1/edit").to route_to("expenses#edit", id: "1")
+      expect(get: "category/expenses/1/edit").to route_to("expenses#edit", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/expenses").to route_to("expenses#create")
+      expect(post: "category/expenses").to route_to("expenses#create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/expenses/1").to route_to("expenses#update", id: "1")
+      expect(put: "category/expenses/1").to route_to("expenses#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/expenses/1").to route_to("expenses#update", id: "1")
+      expect(patch: "category/expenses/1").to route_to("expenses#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/expenses/1").to route_to("expenses#destroy", id: "1")
+      expect(delete: "category/expenses/1").to route_to("expenses#destroy", id: "1")
     end
   end
 end
